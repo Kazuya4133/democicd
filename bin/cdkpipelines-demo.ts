d581e10bbd21c8116ad6ae183c1b91b16c1324f7
mavs4133@gmail.com <mavs4133@gmail.com>
2023-04-14 07:29:45 +0000

    Add Stage

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 import { App } from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+import { CdkpipelinesDemoStage } from '../lib/cdkpipelines-demo-stage';
 
 const app = new App();
 
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
   env: { account: '008458347550', region: 'ap-northeast-1' },
+});
+
+new CdkpipelinesDemoStage(app, 'Dev', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
+
+new CdkpipelinesDemoStage(app, 'Stg', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
 
 app.synth();
